8c89730d187dddc30e5069002b33031ed2c1de68
Taiene Yuki <taieneyamazaki@gmail.com>
2023-05-27 14:04:55 -0300

    css cleanup

--- src/components/progress_bar/progress_bar.js
+++ src/components/progress_bar/progress_bar.js
@@ -3,11 +3,11 @@ import Step from "./step";
 
 const ProgressBar = (props) => {
     return (
-        <div className="stepper-wrapper">
-            {props.steps.map((step) => (
-                <Step children={step} key={step} count={props.count}></Step>
-            ))}
-        </div>
+            <div className="stepper-wrapper">
+                {props.steps.map((step) => (
+                    <Step children={step} key={step} count={props.count}></Step>
+                ))}
+            </div>
     )
 };
 
